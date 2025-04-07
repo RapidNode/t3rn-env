1322787065aaf0007804bee2eb91d760b70eb21f
Akash6222 <akashdubey25274@gmail.com>
2025-04-08 02:03:39 +0530

    Added t3rn_default_rpc.sh

---
+++ t3rn_default_rpc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "https://t3rn-b2n.blockpi.network/v1/rpc/public,https://b2n.rpc.caldera.xyz/http" > l2rn.txt
+echo "https://arbitrum-sepolia.drpc.org,https://sepolia-rollup.arbitrum.io/rpc" > arbt.txt
+echo "https://base-sepolia-rpc.publicnode.com,https://base-sepolia.drpc.org" > bast.txt
+echo "https://sepolia.optimism.io,https://optimism-sepolia.drpc.org" > opst.txt
+echo "https://unichain-sepolia.drpc.org,https://sepolia.unichain.org" > unit.txt
