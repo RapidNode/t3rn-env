d378653c7dda454f44980a784605644e8f391ca2
Akash6222 <akashdubey25274@gmail.com>
2025-04-07 22:45:45 +0530

    Updated t3rn-env.sh

--- t3rn-env.sh
+++ t3rn-env.sh
@@ -10,11 +10,14 @@ PRIVATE_KEY_LOCAL=$(cat private_key.txt)
 ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,unichain-sepolia,l2rn'
 EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
 EXECUTOR_PROCESS_ORDERS_API_ENABLED=false
+EXECUTOR_ENABLE_BATCH_BIDDING=true
+NETWORKS_DISABLED='blast-sepolia'
 RPC_ENDPOINTS='{
     "l2rn": [$(cat l2rn.txt)],
     "arbt": [$(cat arbt.txt)],
     "bast": [$(cat bast.txt)],
     "opst": [$(cat opst.txt)],
-    "unit": [$(cat unit.txt)]
+    "unit": [$(cat unit.txt)],
+    "blst": [$(cat blst.txt)]
 }'
 EOF
